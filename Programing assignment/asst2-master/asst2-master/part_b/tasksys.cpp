1a43714b244ffc676fd47163c18e8473afe58196
Myung-gyu Park <68139415+audrb1999@users.noreply.github.com>
2023-06-02 14:09:18 +0900

    Add files via upload

--- Programing assignment/asst2-master/asst2-master/part_b/tasksys.cpp
+++ Programing assignment/asst2-master/asst2-master/part_b/tasksys.cpp
@@ -1,4 +1,6 @@
 #include "tasksys.h"
+#include <iostream>
+#include <algorithm>
 
 
 IRunnable::~IRunnable() {}
@@ -133,6 +135,20 @@ TaskSystemParallelThreadPoolSleeping::TaskSystemParallelThreadPoolSleeping(int n
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
+
+    // 초기화
+
+    this->num_threads = num_threads;
+    this->current_taskid = 0;
+    this->task_run_mutex = new std::mutex();
+    this->finished_task_mutex = new std::mutex();
+    this->task_run_cr = new std::condition_variable();
+    this->finished_task_cr = new std::condition_variable();
+    this->done_flag = false;
+    // std::map<TaskID, std::set<TaskID>> tasksDep;
+    for (int i = 0; i < num_threads; i++) {
+        this->pool.push_back(std::thread(&TaskSystemParallelThreadPoolSleeping::runThreadParallelThreadPoolSleeping, this, i));
+    }
 }
 
 TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
@@ -142,6 +158,18 @@ TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
+
+    // 
+
+    done_flag = true;
+    task_run_cr->notify_all();
+    for (int i = 0; i < num_threads; i++) {
+        pool[i].join();
+    }
+    delete task_run_mutex;
+    delete finished_task_mutex;
+    delete task_run_cr;
+    delete finished_task_cr;
 }
 
 void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
@@ -152,10 +180,9 @@ void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_tota
     // method in Parts A and B.  The implementation provided below runs all
     // tasks sequentially on the calling thread.
     //
-
-    for (int i = 0; i < num_total_tasks; i++) {
-        runnable->runTask(i, num_total_tasks);
-    }
+    runAsyncWithDeps(runnable, num_total_tasks, {});
+    sync();
+    return;
 }
 
 TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
@@ -166,11 +193,20 @@ TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable* runnabl
     // TODO: CS149 students will implement this method in Part B.
     //
 
-    for (int i = 0; i < num_total_tasks; i++) {
-        runnable->runTask(i, num_total_tasks);
+    // 종속성 그래프를 만들고 실행 가능한 작업에 대해서 비동기적으로 실행
+
+    // 시작
+    if (deps.size() == 0) {
+        printf("\n 현재 id: %d\n", current_taskid);
+        tasks_dep[current_taskid];
     }
 
-    return 0;
+    // task id를 key로 종속성을 값으로 딕셔너리를 만듬
+    for (auto dep: deps) {
+        tasks_dep[current_taskid].insert(dep);
+    }
+    task_id_to_task[current_taskid] = new Task(current_taskid, runnable, num_total_tasks);
+    return current_taskid++;
 }
 
 void TaskSystemParallelThreadPoolSleeping::sync() {
@@ -178,6 +214,109 @@ void TaskSystemParallelThreadPoolSleeping::sync() {
     //
     // TODO: CS149 students will modify the implementation of this method in Part B.
     //
-
+    
+    scanForReadyTasks(); // 작업 실행 및 완료된 작업 찾기
+    bool done_work = false;
+    while (!done_work) { // 모든 작업이 완료될 때까지 실행
+        std::unique_lock<std::mutex> finished_task_lock(*finished_task_mutex);
+        
+        while (finished_tasks.empty()) {
+            finished_task_cr->wait(finished_task_lock);
+        }
+        bool has_more_finished_tasks = true;
+        while (has_more_finished_tasks) {
+            // 완료된 작업을 pop하고 dependencies graph에서 제거
+            auto task_done_id = finished_tasks.front();
+            finished_tasks.pop_front();
+            remaining_tasks.erase(remaining_tasks.find(task_done_id));
+            finished_task_lock.unlock();
+            removeTaskIDFromDependency(task_done_id);
+            finished_task_lock.lock();
+            has_more_finished_tasks = !finished_tasks.empty();
+        }
+        finished_task_lock.unlock();
+        // 독립적인 작업을 찾고 thread를 깨움
+        scanForReadyTasks();
+        finished_task_mutex->lock();
+        done_work = tasks_dep.empty() && remaining_tasks.empty();
+        finished_task_mutex->unlock();
+    }
     return;
 }
+
+void TaskSystemParallelThreadPoolSleeping::runThreadParallelThreadPoolSleeping(int thread_number) {
+    while (!done_flag) {
+        bool done_work = false; 
+        while (!done_work) {
+            std::unique_lock<std::mutex> task_run_lock(*task_run_mutex);
+            // 준비된 작업이 없으면 대기
+            while (runnable_tasks.empty() && !done_flag) {
+                task_run_cr->wait(task_run_lock);
+            }
+            done_work = runnable_tasks.empty();
+            if (done_work) {
+                task_run_lock.unlock();
+                continue;
+            }
+            // pop work and execute
+            auto task = runnable_tasks.front();
+            runnable_tasks.pop_front();
+            task_run_lock.unlock();
+
+            task->runnable->runTask(task->current_task, task->num_total_tasks);
+
+            // 작업 카운터를 감소시키고 완료된 대기열에 추가
+            finished_task_mutex->lock();
+            remaining_tasks[task->id]--;
+            if (remaining_tasks[task->id] <= 0) {
+                finished_tasks.push_back(task->id);
+                finished_task_mutex->unlock();
+                finished_task_cr->notify_one();
+            } else {
+                finished_task_mutex->unlock();
+            }
+        }
+    }
+}
+
+void TaskSystemParallelThreadPoolSleeping::scanForReadyTasks() {
+   for (auto it = tasks_dep.begin(); it != tasks_dep.end();) { 
+        auto tasks = it->second;
+        if (tasks.empty()) {
+            Task* t = task_id_to_task[it->first];
+            remaining_tasks[t->id] = t->num_total_tasks;
+            task_run_mutex->lock();
+            for (int i = 0; i < t->num_total_tasks; i++) {
+                runnable_tasks.push_back(new RunnableTask(t->id, i, t->runnable, t->num_total_tasks));
+            }
+            task_run_mutex->unlock();
+            task_run_cr->notify_all();
+            it = tasks_dep.erase(it);
+       } else {
+           ++it;
+       }
+   }
+}
+
+void TaskSystemParallelThreadPoolSleeping::removeTaskIDFromDependency(TaskID finished_task) {
+   for (auto it = tasks_dep.begin(); it != tasks_dep.end();it++) { 
+        it->second.erase(finished_task);
+   } 
+}
+
+void TaskSystemParallelThreadPoolSleeping::printSet(std::set<TaskID> tasks) {
+    for (auto task_it = tasks.begin(); task_it != tasks.end();task_it++) {
+        std::cout << *task_it << ", ";
+    }
+}
+
+void TaskSystemParallelThreadPoolSleeping::printTasksDep() {
+   for (auto it = tasks_dep.begin(); it != tasks_dep.end();it++) { 
+        auto tasks = it->second;
+        std::cout << it->first << ": ";
+        printSet(tasks);
+        std::cout << std::endl;
+   }  
+   std::cout << "==========" << std::endl;
+}
+
--- Programing assignment/asst2-master/asst2-master/part_b/tasksys.h
+++ Programing assignment/asst2-master/asst2-master/part_b/tasksys.h
@@ -2,6 +2,14 @@
 #define _TASKSYS_H
 
 #include "itasksys.h"
+#include <map>
+#include <thread>
+#include <tuple>
+#include <deque>
+#include <iostream>
+#include <mutex>
+#include <set>
+#include <condition_variable>
 
 /*
  * TaskSystemSerial: This class is the student's implementation of a
@@ -19,12 +27,7 @@ class TaskSystemSerial: public ITaskSystem {
         void sync();
 };
 
-/*
- * TaskSystemParallelSpawn: This class is the student's implementation of a
- * parallel task execution engine that spawns threads in every run()
- * call.  See definition of ITaskSystem in itasksys.h for documentation
- * of the ITaskSystem interface.
- */
+
 class TaskSystemParallelSpawn: public ITaskSystem {
     public:
         TaskSystemParallelSpawn(int num_threads);
@@ -36,12 +39,7 @@ class TaskSystemParallelSpawn: public ITaskSystem {
         void sync();
 };
 
-/*
- * TaskSystemParallelThreadPoolSpinning: This class is the student's
- * implementation of a parallel task execution engine that uses a
- * thread pool. See definition of ITaskSystem in itasksys.h for
- * documentation of the ITaskSystem interface.
- */
+
 class TaskSystemParallelThreadPoolSpinning: public ITaskSystem {
     public:
         TaskSystemParallelThreadPoolSpinning(int num_threads);
@@ -53,14 +51,75 @@ class TaskSystemParallelThreadPoolSpinning: public ITaskSystem {
         void sync();
 };
 
-/*
- * TaskSystemParallelThreadPoolSleeping: This class is the student's
- * optimized implementation of a parallel task execution engine that uses
- * a thread pool. See definition of ITaskSystem in
- * itasksys.h for documentation of the ITaskSystem interface.
- */
+class Task {
+   public:
+        TaskID id;
+        IRunnable* runnable;
+        int num_total_tasks;
+
+        Task(TaskID id, IRunnable* runnable, int num_total_tasks) {
+            this->id = id;
+            this->runnable = runnable;
+            this->num_total_tasks = num_total_tasks;
+        }
+        Task(const Task &t) {
+            this->id = t.id;
+            this->runnable = t.runnable;
+            this->num_total_tasks = t.num_total_tasks;
+        }
+        friend std::ostream& operator<< (std::ostream &out, Task const& task) {
+            return out << "<id: " << task.id << ", total: " << task.num_total_tasks << ">" << std::endl;
+        }
+};
+
+// 실행이 가능한 작업
+
+class RunnableTask {
+   public:
+        TaskID id;
+        IRunnable* runnable;
+        int current_task;
+        int num_total_tasks;
+
+        RunnableTask(TaskID id, TaskID current_task, IRunnable* runnable, int num_total_tasks) {
+            this->id = id;
+            this->runnable = runnable;
+            this->current_task = current_task;
+            this->num_total_tasks = num_total_tasks;
+        }
+        RunnableTask(const RunnableTask &t) {
+            this->id = t.id;
+            this->runnable = t.runnable;
+            this->current_task = t.current_task;
+            this->num_total_tasks = t.num_total_tasks;
+        }
+        friend std::ostream& operator<< (std::ostream &out, RunnableTask const& task) {
+            return out << "<id: " << task.id << ", current: " << task.current_task << ", total: " << task.num_total_tasks << ">" << std::endl;
+        }
+};
+
+
 class TaskSystemParallelThreadPoolSleeping: public ITaskSystem {
     public:
+        bool done_flag;
+        int num_threads;
+        int current_taskid;
+        // task dependencies
+        std::map<TaskID, std::set<TaskID>> tasks_dep;
+        // map from taskid -> task detail
+        std::map<TaskID, Task*> task_id_to_task;
+        // map to track how many tasks left for each task id
+        std::map<TaskID, int> remaining_tasks;
+        // queue for ready tasks
+        std::deque<RunnableTask*> runnable_tasks;
+        // queue for finished tasks to be removed from dependencies
+        std::deque<TaskID> finished_tasks;
+        std::vector<std::thread> pool;
+        std::mutex* task_run_mutex; 
+        std::mutex* finished_task_mutex;
+        std::condition_variable* task_run_cr;
+        std::condition_variable* finished_task_cr;
+
         TaskSystemParallelThreadPoolSleeping(int num_threads);
         ~TaskSystemParallelThreadPoolSleeping();
         const char* name();
@@ -68,6 +127,11 @@ class TaskSystemParallelThreadPoolSleeping: public ITaskSystem {
         TaskID runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
                                 const std::vector<TaskID>& deps);
         void sync();
+        void runThreadParallelThreadPoolSleeping(int thread_number);
+        void scanForReadyTasks();
+        void removeTaskIDFromDependency(TaskID i);
+        void printTasksDep();
+        void printSet(std::set<TaskID> tasks);
 };
 
 #endif
